eae9ac481f1f4a047af40ee042b87f8dccc0e64f
Lucas Oliveira <oliveiraborgeslucas@gmail.com>
2019-03-07 07:47:16 +0000

    first commit

---
+++ DecimalComparator/src/DecimalComparator.java
@@ -0,0 +1,11 @@
+public class DecimalComparator
+{
+
+	public static boolean areEqualByThreeDecimalPlaces(double first, double second)
+	{
+
+		return ((int) (first * 1000)) == ((int) (second * 1000));
+
+	}
+
+}
---
+++ EqualSumChecker/src/EqualSumChecker.java
@@ -0,0 +1,11 @@
+
+public class EqualSumChecker
+{
+
+	public static boolean hasEqualSum(int a, int b, int c)
+	{
+
+		return (a + b) == c;
+
+	}
+}
