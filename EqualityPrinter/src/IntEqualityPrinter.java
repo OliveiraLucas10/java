8bce209e2008dc4884d36745077b885c34c5d3d3
Lucas Oliveira <oliveiraborgeslucas@gmail.com>
2019-03-15 18:16:31 +0000

    changin variables name

--- EqualityPrinter/src/IntEqualityPrinter.java
+++ EqualityPrinter/src/IntEqualityPrinter.java
@@ -4,9 +4,9 @@ public class IntEqualityPrinter
 
 	public static void printEqual(int a, int b, int c)
 	{
-		boolean a_is_b = (a == b);
-		boolean a_is_c = (a == c);
-		boolean b_is_c = (b == c);
+		boolean aIsB = (a == b);
+		boolean aIsC = (a == c);
+		boolean bIsC = (b == c);
 
 		if (a < 0 || b < 0 || c < 0)
 		{
@@ -14,11 +14,11 @@ public class IntEqualityPrinter
 		}
 		else
 		{
-			if (a_is_b && a_is_c)
+			if (aIsB && aIsC)
 			{
 				System.out.println("All numbers are equal");
 			}
-			else if (!a_is_b && !a_is_c && !b_is_c)
+			else if (!aIsB && !aIsC && !bIsC)
 			{
 				System.out.println("All numbers are different");
 			}
